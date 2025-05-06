51708e29aa5fef98a1576e2a12f1c433bac11f94
HakimYu <yhjredin@gmail.com>
2025-05-06 14:16:57 +0800

    feat(img): 图片大小压缩，加快访问速度

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 // https://vite.dev/config/
 
 const __filename = fileURLToPath(import.meta.url)
 const _dirname = dirname(__filename)
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteImageOptimizer(IMAGE_OPTIMIZER_OPTIONS)],
   resolve: {
     alias: {
       '@': resolve(_dirname, './src')
@@ -25,3 +26,66 @@ export default defineConfig(({ mode }) => ({
     } : undefined
   }
 }))
+const IMAGE_OPTIMIZER_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  ansiColors: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 80,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 80,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 100,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    lossless: true,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+  cache: false,
+  cacheLocation: undefined,
+};
